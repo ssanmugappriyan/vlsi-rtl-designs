63e508c2b83ee0f53fec1875205154316202064c
ssanmugappriyan <sanmugappriyan.ee@gmail.com>
2026-05-23 17:30:14 +0530

    Add testbench for logic_gates module

---
+++ Verilog/tb_logic_gates.v
@@ -0,0 +1,29 @@
+`timescale 1ns/1ps
+module logic_gate_tb;
+  reg a;
+  reg b;
+
+  wire and_out;
+  wire or_out;
+
+logic_gates uut(
+  .a(a),
+  .b(b),
+
+  .and_out(and_out),
+  .or_out(or_out)
+);
+
+initial begin
+a=0;b=0;
+#10;
+a=0;b=1;
+#10;
+a=1;b=0;
+#10;
+a=1;b=1;
+#10;
+
+$stop;
+end
+endmodule
